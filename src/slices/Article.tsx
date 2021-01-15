680ed2669f5883ba55d8878d16e193a6e253c7bd
Lukas Wochner <lukas.wochner@blateral.com>
2021-01-15 14:49:50 +0100

    adding scripts

--- src/slices/Article.tsx
+++ src/slices/Article.tsx
@@ -1,37 +1,37 @@
 import {
-  PrismicBoolean,
-  PrismicHeading,
-  PrismicRichText,
-  PrismicSlice,
-  linkResolver,
-} from "../utils/prismic";
+    PrismicBoolean,
+    PrismicHeading,
+    PrismicRichText,
+    PrismicSlice,
+    linkResolver,
+} from '../utils/prismic';
 
-import React from "react";
-import { RichText } from "prismic-dom";
-import { Article } from "@blateral/b.kit";
+import React from 'react';
+import { RichText } from 'prismic-dom';
+import { Article } from '@blateral/b.kit';
 
-export interface ArticleSliceType extends PrismicSlice<"artikel"> {
-  primary: {
-    suptitle?: PrismicHeading;
-    title?: PrismicHeading;
-    text?: PrismicRichText;
-    asideText?: PrismicRichText;
-    isInverted?: PrismicBoolean;
-  };
+export interface ArticleSliceType extends PrismicSlice<'artikel'> {
+    primary: {
+        suptitle?: PrismicHeading;
+        title?: PrismicHeading;
+        text?: PrismicRichText;
+        asideText?: PrismicRichText;
+        isInverted?: PrismicBoolean;
+    };
 }
 
 const ArticleSlice: React.FC<ArticleSliceType> = ({
-  primary: { suptitle, title, text, asideText, isInverted },
+    primary: { suptitle, title, text, asideText, isInverted },
 }) => {
-  return (
-    <Article
-      title={RichText.asText(title)}
-      superTitle={RichText.asText(suptitle)}
-      text={RichText.asHtml(text, linkResolver)}
-      asideText={RichText.asHtml(text, linkResolver)}
-      isInverted={isInverted}
-    />
-  );
+    return (
+        <Article
+            title={RichText.asText(title)}
+            superTitle={RichText.asText(suptitle)}
+            text={RichText.asHtml(text, linkResolver)}
+            asideText={RichText.asHtml(text, linkResolver)}
+            isInverted={isInverted}
+        />
+    );
 };
 
 export default ArticleSlice;
