392fc3e5fe7a2ea7e5df30f595c20a970e7d911c
Maja Merk <maja.merk@blateral.com>
2021-01-15 14:44:01 +0100

    Being stupid

--- src/slices/Article.tsx
+++ src/slices/Article.tsx
@@ -21,8 +21,11 @@ export interface ArticleSliceType extends PrismicSlice<'artikel'> {
         text?: PrismicRichText;
         aside_text?: PrismicRichText;
         is_inverted?: PrismicBoolean;
-        primary_action?: { link?: PrismicLink | string; label?: string };
-        secondary_action?: { link?: PrismicLink | string; label?: string };
+        bg_mode?: 'full' | 'splitted';
+        primary_link?: PrismicLink | string;
+        secondary_link?: PrismicLink | string;
+        primary_label?: string;
+        secondary_label?: string;
     };
 }
 
@@ -33,12 +36,16 @@ const ArticleSlice: React.FC<ArticleSliceType> = ({
         text,
         aside_text,
         is_inverted,
-        primary_action,
-        secondary_action,
+        bg_mode,
+        primary_link,
+        primary_label,
+        secondary_link,
+        secondary_label,
     },
 }) => {
     return (
         <Article
+            bgMode={bg_mode}
             title={RichText.asText(title)}
             superTitle={RichText.asText(suptitle)}
             text={RichText.asHtml(text, linkResolver)}
@@ -46,18 +53,18 @@ const ArticleSlice: React.FC<ArticleSliceType> = ({
             isInverted={is_inverted}
             primaryAction={(is_inverted) => (
                 <Button.View
-                    href={resolveUnknownLink(primary_action?.link) || ''}
+                    href={resolveUnknownLink(primary_link) || ''}
                     isInverted={is_inverted}
                 >
-                    <Button.Label>{primary_action?.label}</Button.Label>
+                    <Button.Label>{primary_label}</Button.Label>
                 </Button.View>
             )}
             secondaryAction={(isInverted) => (
                 <Button.View
-                    href={resolveUnknownLink(secondary_action?.link) || ''}
+                    href={resolveUnknownLink(secondary_link) || ''}
                     isInverted={isInverted}
                 >
-                    <Button.Label>{secondary_action?.label}</Button.Label>
+                    <Button.Label>{secondary_label}</Button.Label>
                 </Button.View>
             )}
         />
