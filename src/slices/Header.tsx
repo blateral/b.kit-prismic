ccac0e7669289e14e5068dfe30016663b5c6f6cc
Falco Böhnke <falco.boehnke@blateral.com>
2021-01-27 12:36:57 +0100

    Fixing tc issues

--- src/slices/Header.tsx
+++ src/slices/Header.tsx
@@ -1,26 +1,13 @@
-import { Footer, Header } from '@blateral/b.kit';
 import {
-    PrismicBoolean,
-    PrismicHeading,
-    PrismicImage,
     PrismicKeyText,
     PrismicLink,
-    PrismicNavigationSliceType,
-    PrismicRichText,
     PrismicSettingsPage,
     PrismicSlice,
-    linkResolver,
-    resolveUnknownLink,
 } from '../utils/prismic';
 
+import { Header } from '@blateral/b.kit';
 // import { FactList } from '@blateral/b.kit';
 import React from 'react';
-import { RichText } from 'prismic-dom';
-
-interface BottomLink {
-    label?: PrismicKeyText;
-    href?: PrismicLink;
-}
 
 export interface HeaderSliceType extends PrismicSlice<'header'> {
     primary: {
@@ -42,39 +29,40 @@ export interface HeaderSliceType extends PrismicSlice<'header'> {
 }
 
 export const HeaderSlice: React.FC<HeaderSliceType> = ({
-    primary: { primary_label, primary_link, secondary_label, secondary_link },
-    settingsPage: { data },
+    // primary: { primary_label, primary_link, secondary_label, secondary_link },
+    settingsPage,
 }) => {
+    settingsPage;
     return <Header title={'Test'} />;
 };
 
-const mapSocials = ({
-    youtube,
-    facebook,
-    instagram,
-}: {
-    youtube?: PrismicLink;
-    facebook?: PrismicLink;
-    instagram?: PrismicLink;
-}): { href: string; icon: any }[] => {
-    const socials = [];
+// const mapSocials = ({
+//     youtube,
+//     facebook,
+//     instagram,
+// }: {
+//     youtube?: PrismicLink;
+//     facebook?: PrismicLink;
+//     instagram?: PrismicLink;
+// }): { href: string; icon: any }[] => {
+//     const socials = [];
 
-    youtube &&
-        socials.push({
-            href: resolveUnknownLink(youtube) || '',
-            icon: <img src="https://via.placeholder.com/50" />,
-        });
-    facebook &&
-        socials.push({
-            href: resolveUnknownLink(facebook) || '',
-            icon: <img src="https://via.placeholder.com/50" />,
-        });
+//     youtube &&
+//         socials.push({
+//             href: resolveUnknownLink(youtube) || '',
+//             icon: <img src="https://via.placeholder.com/50" />,
+//         });
+//     facebook &&
+//         socials.push({
+//             href: resolveUnknownLink(facebook) || '',
+//             icon: <img src="https://via.placeholder.com/50" />,
+//         });
 
-    instagram &&
-        socials.push({
-            href: resolveUnknownLink(instagram) || '',
-            icon: <img src="https://via.placeholder.com/50" />,
-        });
+//     instagram &&
+//         socials.push({
+//             href: resolveUnknownLink(instagram) || '',
+//             icon: <img src="https://via.placeholder.com/50" />,
+//         });
 
-    return socials;
-};
+//     return socials;
+// };
