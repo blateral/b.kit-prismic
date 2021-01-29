0105842707ffc02cbecb4bf5f35442ca021cba73
Falco <falco.boehnke@gmail.com>
2021-01-29 15:06:33 +0100

    Adding header export

--- src/index.ts
+++ src/index.ts
@@ -12,5 +12,6 @@ export * from 'slices/FeatureList';
 export * from 'slices/IconList';
 
 export * from 'slices/carousels/ImageCarousel';
+export * from 'slices/Header';
 
 export * from 'slices/Footer';
