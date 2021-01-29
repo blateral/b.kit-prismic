e7504664e973a14d78ebd0c283c0ce3f8b3cb5d2
Falco <falco.boehnke@gmail.com>
2021-01-29 16:11:25 +0100

    Adding headerslice wip

--- src/slices/Header.tsx
+++ src/slices/Header.tsx
@@ -63,16 +63,79 @@ export const HeaderSlice: React.FC<HeaderSliceType> = ({
     primaryAction,
     secondaryAction,
 }) => {
-    settingsPage;
+    const settings = settingsPage?.data;
+
+
+    console.log("SETTINGS", settings);
+    console.log("Nav items map", settings?.main_nav?.map((navItem, index) => {
+        return {
+            id: `main-nav-group-${index}`,
+            name: navItem.primary.name,
+            isSmall: navItem.primary.is_small,
+            items: navItem?.items?.map((item, subindex)=>{
+                return {
+                    id: `nav-group-${subindex}`,
+                    label: item.label || "",
+                    link: {
+                        href: resolveUnknownLink(item.link) || ""
+                    }
+                }
+            })
+        }
+    }))
     return (
         <Header
             size={size ? (size as 'small') || 'full' : 'full'}
             images={items.map((image) => {
-                return { small: image.url || '' };
+                return {
+                    small: image.url || '',
+                    medium: image.url || '',
+                    large: image.url,
+                    semilarge: image.url,
+                    xlarge: image.url,
+                };
             })}
             titleAs={title && getHtmlElementFromPrismicType(title[0] as any)}
             title={getText(title)}
-            
+            badge={{
+                content: (
+                    <img
+                        src="https://via.placeholder.com/392x392/?text=badge"
+                        style={{ height: '100%', width: '100%' }}
+                    />
+                ),
+                showOnMobile: true,
+            }}
+            menu={{
+                primaryCta: primaryAction,
+                secondaryCta: secondaryAction,
+                logo: {
+                    link: resolveUnknownLink(settings?.logo_href) || '',
+                },
+                isNavInverted: nav_inverted,
+                socials: settings?.socials?.map(social=>{
+                    return {
+                        href: resolveUnknownLink(social.link) || "",
+                        icon: social.platform 
+                    }
+                }),
+                navItems: settings?.main_nav?.map((navItem, index) => {
+                    return {
+                        id: `main-nav-group-${index}`,
+                        items: navItem?.items?.map((item, subindex)=>{
+                            return {
+                                id: `nav-group-${subindex}`,
+                                label: item.label || "",
+                                link: {
+                                    href: resolveUnknownLink(item.link) || ""
+                                }
+                            }
+                        })
+                    }
+                })
+                
+                
+            }}
             primaryCta={(isInverted) =>
                 primaryAction &&
                 primaryAction(
