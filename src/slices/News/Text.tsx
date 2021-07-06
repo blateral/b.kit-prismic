ea7654239a4b0edd67c87bdadd37876ec9f5497f
Falco Böhnke <falco.boehnke@blateral.com>
2021-07-06 11:41:06 +0200

    Cleanup

--- src/slices/News/Text.tsx
+++ src/slices/News/Text.tsx
@@ -9,31 +9,23 @@ import {
     resolveUnknownLink,
     getText,
     getHtmlText,
-} from '../../utils/prismic';
+} from 'utils/prismic';
 
-import { AliasSelectMapperType } from '../../utils/mapping';
 import { NewsText } from '@blateral/b.kit';
 import React from 'react';
-
-type BgMode =
-    | 'full'
-    | 'half-left'
-    | 'half-right'
-    | 'larger-left'
-    | 'larger-right';
 
 export interface NewsTextSliceType extends PrismicSlice<'NewsText'> {
     primary: {
         is_active?: PrismicBoolean;
         text?: PrismicRichText;
         is_inverted?: PrismicBoolean;
+        has_background?: PrismicBoolean;
         primary_link?: PrismicLink;
         secondary_link?: PrismicLink;
         primary_label?: PrismicKeyText;
         secondary_label?: PrismicKeyText;
     };
-    // helpers to define component elements outside of slice
-    bgModeSelectAlias?: AliasSelectMapperType<BgMode>;
+
     primaryAction?: (props: {
         isInverted?: boolean;
         label?: string;
@@ -52,6 +44,7 @@ export const NewsTextSlice: React.FC<NewsTextSliceType> = ({
     primary: {
         text,
         is_inverted,
+        has_background,
         primary_link,
         primary_label,
         secondary_link,
@@ -64,6 +57,7 @@ export const NewsTextSlice: React.FC<NewsTextSliceType> = ({
         <NewsText
             text={getHtmlText(text)}
             isInverted={is_inverted}
+            hasBack={has_background}
             primaryAction={(isInverted) =>
                 primaryAction &&
                 primaryAction({
